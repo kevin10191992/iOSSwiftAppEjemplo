25984350530fb707c7845ee10b4d371862047191
kevin <kevin10191992>
2025-06-07 11:13:38 -0500

    Initial Commit

---
+++ appejemplokpr/appejemplokprApp.swift
@@ -0,0 +1,17 @@
+//
+//  appejemplokprApp.swift
+//  appejemplokpr
+//
+//  Created by Kevin Perez Rondon on 7/06/25.
+//
+
+import SwiftUI
+
+@main
+struct appejemplokprApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ appejemplokprTests/appejemplokprTests.swift
@@ -0,0 +1,16 @@
+//
+//  appejemplokprTests.swift
+//  appejemplokprTests
+//
+//  Created by Kevin Perez Rondon on 7/06/25.
+//
+
+import Testing
+
+struct appejemplokprTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
